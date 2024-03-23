9645ed4d1116a5f81006def4b8d092ad939ed90f
Leonardo Bai <69223422+LeonardoBai12@users.noreply.github.com>
2024-03-22 23:45:22 -0300

    Melhorias de UI e UX

---
+++ lib/utils/blur_effect.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class BlurEffect extends StatelessWidget {
+  const BlurEffect({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+        child: Container(
+          color: Colors.transparent,
+        ),
+      ),
+    );
+  }
+}
